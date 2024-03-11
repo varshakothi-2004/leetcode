306f83b502392d5a50ea26f6e10c8f5fe4275036
varshakothi-2004 <127662298+varshakothi-2004@users.noreply.github.com>
2024-03-11 20:03:50 +0530

    Create swapping nodes in a linked list.py

---
+++ linked list/swapping nodes in a linked list.py
@@ -0,0 +1,21 @@
+class Solution:
+    def swapNodes(self, head: ListNode | None, k: int) -> ListNode | None:
+        
+        def iter_ll(ll: ListNode | None) -> Iterator[ListNode]:
+            while ll: yield ll; ll = ll.next
+        
+        T = TypeVar('T')
+        def last(xs: Iterable[T]) -> T:
+            return reduce(lambda _, x: x, xs)
+        
+        sentinal_head = ListNode(next=head)
+        xs, ys = iter_ll(sentinal_head), iter_ll(sentinal_head)
+
+        k_begin = next(islice(ys, k - 1, None))
+        k_end = last(zip(xs, ys))[0]
+
+        b, e = k_begin.next, k_end.next
+        k_begin.next, k_end.next = e, b
+        b.next, e.next = e.next, b.next
+        
+        return sentinal_head.next
